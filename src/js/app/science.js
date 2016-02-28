fa6308e95cf047a3cd26ee86a5b88881726d71d1
WineBewes <wine.bewes@gmail.com>
2016-02-28 21:50:55 +0100

    1e commit

--- src/js/app/science.js
+++ src/js/app/science.js
@@ -1,4 +1,28 @@
 /**
  * Created by Erwin on 28/02/16.
  */
-var app = angular.module("science", []);
+(function() {
+
+    var app = angular.module('science', []);
+
+    app.controller('CircleController', function(){
+
+        return {
+
+            'diameter' : 5 * 2,
+            'omtrek' : 5 * Math.PI * 2,
+            'oppervlakte' : Math.pow(5, 2) * Math.PI
+
+        };
+    });
+
+    app.controller('SphereController', function(){
+
+        return {
+
+            'oppervlakte' : 4 * Math.pow(5, 2) * Math.PI,
+            'volume' :   4 * Math.pow(5, 3) * Math.PI / 3
+        };
+    });
+
+})();
